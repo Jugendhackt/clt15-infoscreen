82017a75b8bce7100b2a8364dae57d7ccc180965
pajowu <github@ca.pajowu.de>
2016-03-13 02:06:56 +0200

    small changes

---
+++ generate.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import os, shutil, json
+"""from jinja2 import Environment, FileSystemLoader
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_ENVIRONMENT = Environment(
+    autoescape=False,
+    loader=FileSystemLoader(os.path.join(PATH, 'templates')),
+    trim_blocks=False)
+
+def render_template(template_filename, context):
+    return TEMPLATE_ENVIRONMENT.get_template(template_filename).render(context)"""
+
+
+def create_index_html():
+    fname = "out/output.html"
+    context = {}
+    with open("json/projekte/projekte.json") as projectfile:
+        context["projects"] = json.load(projectfile)
+    with open("json/pictures/pictures.json") as projectfile:
+        context["pictures"] = json.load(projectfile)
+    with open("js/project.js") as tempfile:
+        tmp = tempfile.read()
+        tmp = tmp.replace("{{CONTEXT}}", json.dumps(context))
+        with open("out/js/project.js", "w") as outfile:
+            outfile.write(tmp)
+    shutil.copy("templates/index.html", "out/output.html")
+
+def prepare():
+    if os.path.isdir("out"):
+        shutil.rmtree("out")
+    os.mkdir("out")
+
+def copy_assets():
+    shutil.copytree("css", "out/css")
+    shutil.copytree("js", "out/js")
+
+def main():
+    prepare()
+    copy_assets()
+    create_index_html()
+
+
+########################################
+
+if __name__ == "__main__":
+    main()
---
+++ js/project.js
@@ -0,0 +1,39 @@
+var app = angular.module('clt16', []).filter('username', function() {
+        return function(input) {
+            return input.split("/")[-1];
+        }
+    });
+app.controller('MainController', ['$scope', function ($scope) {
+    $scope.init = function() {
+        $scope.context = {{CONTEXT}};
+        $scope.current_picture = $scope.context.pictures[0];
+        $scope.current_picture_id = 0;
+        $scope.project = $scope.context.projects[0];
+        $scope.project_id = 0;
+        setTimeout($scope.nextProject, 10000);
+        setTimeout($scope.nextImage, 10000);
+    }
+    $scope.nextImage = function() {
+        $scope.current_picture_id += 1;
+        $scope.current_picture_id = $scope.current_picture_id % $scope.context.pictures.length;
+        $scope.current_picture = $scope.context.pictures[$scope.current_picture_id];
+        $scope.$apply();
+        setTimeout($scope.nextImage, 10000);
+    }
+    $scope.nextProject = function() {
+        $scope.project_id += 1;
+        $scope.project_id = $scope.project_id % $scope.context.projects.length;
+        $scope.project = $scope.context.projects[$scope.project_id];
+        $scope.$apply();
+        setTimeout($scope.nextProject, 100000);
+    }
+    $scope.init();
+
+}]);
+
+if (!Array.prototype.last){
+    Array.prototype.last = function(){
+        return this[this.length - 1];
+    };
+};
+String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
